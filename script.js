bd4f48c1d44037ebc0b70262e49d4b9933e942f6
abc <abc@example.com>
2023-02-14 21:04:10 -0500

    got the api key working and data in console

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-var searchBtn = $('searchBtn');
-var userInput = $('searchInput');
+var searchBtn = $('#searchBtn');
+var userInput = $('#searchInput');
 
 
 
 function getApi()  {
-   var getURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + userInput.value + '&APPID=3eb6b9a5a18a639145c452fbac9d924a';
+   var getURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + userInput.val() + '&APPID=3eb6b9a5a18a639145c452fbac9d924a';
     fetch(getURL)
         .then(function (response) {
         return response.json();
         })
         .then(function (data) {
             console.log(data);
+            for (i=0;i<5;i++) {
+                $("#mainTemp" +(i++)+"temp").innerHTML = Number(data.list[i].main.temp)
+              }
         });
 }
 
-searchBtn.on('click', getApi());
+searchBtn.on('click', getApi);
